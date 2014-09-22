63bbd5e44d9afba26019d47304dfc33e8ffc58eb
guidofenix <guido.barbaglia@fao.org>
2014-09-22 16:26:54 +0200

    Switch between section and module.

--- js/tiles-manager/tiles-manager.js
+++ js/tiles-manager/tiles-manager.js
@@ -37,39 +37,52 @@ define(['jquery',
         };
 
         var show_tiles = function(tile_code) {
-            if (tiles_configuration[tile_code]['type'] == 'section') {
-                $('#tiles_container').empty();
-                for (var i = 0; i < tiles_configuration[tile_code]['tiles'].length; i += 2) {
-                    var child_1 = tiles_configuration[tiles_configuration[tile_code]['tiles'][i]];
-                    var view = {};
-                    var template = null;
-                    var render = null;
-                    if (i + 1 < tiles_configuration[tile_code]['tiles'].length) {
-                        var child_2 = tiles_configuration[tiles_configuration[tile_code]['tiles'][i + 1]];
-                        view = {
-                            tile_img_src_1: CONFIG.url_images + CONFIG.lang + child_1.img,
-                            tile_img_src_2: CONFIG.url_images + CONFIG.lang + child_2.img,
-                            tile_2_id: child_1.id,
-                            tile_3_id: child_2.id
-                        };
-                        template = $(templates).filter('#tile_double_row_structure').html();
-                        render = Mustache.render(template, view);
-                    } else {
-                        view = {
-                            tile_img_src_1: CONFIG.url_images + CONFIG.lang + child_1.img,
-                            tile_1_id: child_1.id
-                        };
-                        template = $(templates).filter('#tile_single_row_structure').html();
-                        render = Mustache.render(template, view);
-                    }
-                    $('#tiles_container').append(render);
+            switch (tiles_configuration[tile_code]['type']) {
+                case 'section':
+                    show_section(tile_code);
+                    break;
+                case 'module':
+                    show_module(tile_code);
+                    break;
+            }
+        };
+
+        var show_module = function(tile_code) {
+            $('#tiles_container').empty();
+        };
+
+        var show_section = function(tile_code) {
+            $('#tiles_container').empty();
+            for (var i = 0; i < tiles_configuration[tile_code]['tiles'].length; i += 2) {
+                var child_1 = tiles_configuration[tiles_configuration[tile_code]['tiles'][i]];
+                var view = {};
+                var template = null;
+                var render = null;
+                if (i + 1 < tiles_configuration[tile_code]['tiles'].length) {
+                    var child_2 = tiles_configuration[tiles_configuration[tile_code]['tiles'][i + 1]];
+                    view = {
+                        tile_img_src_1: CONFIG.url_images + CONFIG.lang + child_1.img,
+                        tile_img_src_2: CONFIG.url_images + CONFIG.lang + child_2.img,
+                        tile_2_id: child_1.id,
+                        tile_3_id: child_2.id
+                    };
+                    template = $(templates).filter('#tile_double_row_structure').html();
+                    render = Mustache.render(template, view);
+                } else {
+                    view = {
+                        tile_img_src_1: CONFIG.url_images + CONFIG.lang + child_1.img,
+                        tile_1_id: child_1.id
+                    };
+                    template = $(templates).filter('#tile_single_row_structure').html();
+                    render = Mustache.render(template, view);
                 }
-                for (var i = 0; i < tiles_configuration[tile_code]['tiles'].length; i++) {
-                    var child = tiles_configuration[tiles_configuration[tile_code]['tiles'][i]];
-                    $('#' + child.id).click(function () {
-                        show_tiles(this.id);
-                    });
-                }
+                $('#tiles_container').append(render);
+            }
+            for (var i = 0; i < tiles_configuration[tile_code]['tiles'].length; i++) {
+                var child = tiles_configuration[tiles_configuration[tile_code]['tiles'][i]];
+                $('#' + child.id).click(function () {
+                    show_tiles(this.id);
+                });
             }
         };
 
