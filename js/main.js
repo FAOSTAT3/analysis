ad12efe304d45b99cc7e1e3fabf776d63fdaea12
Guido Barbaglia <guido.barbaglia@gmail.com>
2014-11-06 17:58:38 +0100

    Stand alone configuration.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
+var repository = '//fenixapps.fao.org/repository/js/';
+
 require.config({
 
-    baseUrl: 'http://168.202.28.57:8080',
+    baseUrl: 'js/libs',
 
     paths: {
-        bootstrap: '//fenixapps.fao.org/repository/js/bootstrap/3.2/js/bootstrap.min',
-        chosen: '//fenixapps.fao.org/repository/js/chosen/1.0.0/chosen.jquery.min',
-        highcharts: '//fenixapps.fao.org/repository/js/highcharts/4.0.4/js/highcharts',
-        jquery: '//code.jquery.com/jquery-1.10.1.min',
-        mustache: '//cdnjs.cloudflare.com/ajax/libs/mustache.js/0.8.1/mustache',
+        'bootstrap': '//fenixapps.fao.org/repository/js/bootstrap/3.2/js/bootstrap.min',
+        'chosen': '//fenixapps.fao.org/repository/js/chosen/1.0.0/chosen.jquery.min',
+        'highcharts': '//fenixapps.fao.org/repository/js/highcharts/4.0.4/js/highcharts',
+        'jquery': '//code.jquery.com/jquery-1.10.1.min',
+        'mustache': '//cdnjs.cloudflare.com/ajax/libs/mustache.js/0.8.1/mustache',
         'jquery.power.tip': '//fenixapps.fao.org/repository/js/jquery.power.tip/1.1.0/jquery.powertip.min',
-        'text': 'analysis/js/libs/text',
-        F3_CHART: 'analysis/js/libs/commons/f3-chart',
-        GHG_QA_QC: 'analysis/js/ghg-qa-qc/ghg-qa-qc',
-        GHG_OVERVIEW: 'analysis/js//ghg-overview/ghg-overview',
-        TILESMGR: 'analysis/js/tiles-manager/tiles-manager',
-        'jquery.sticky': 'stickyjs/1.0/jquery.sticky'
+        'F3_CHART': 'analysis/js/libs/commons/f3-chart',
+        'GHG_QA_QC': 'analysis/js/ghg-qa-qc/ghg-qa-qc',
+        'GHG_OVERVIEW': 'analysis/js//ghg-overview/ghg-overview',
+        'TILESMGR': '../tiles-manager/tiles-manager',
+        'jquery.sticky': 'stickyjs/1.0/jquery.sticky',
+        'ANALYSIS_GHG_QA_QC': '../ghg-qa-qc/ghg-qa-qc',
+        'highcharts': repository + 'highcharts/4.0.4/js/highcharts',
+        'highcharts_exporting' : repository + 'highcharts/4.0.4/js/modules/exporting'
     },
 
     shim: {
@@ -30,6 +34,6 @@ require.config({
 
 });
 
-require(["TILESMGR"], function () {
-    TILESMGR().init()
+require(["TILESMGR"], function (TILESMGR) {
+    TILESMGR.init()
 });
