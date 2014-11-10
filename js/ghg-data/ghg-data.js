98a004d0c5cd3095a50af582086a67deb8f2bf10
Kalimaha <guido.barbaglia@gmail.com>
2014-11-10 12:01:39 +0100

    New 'Georeferenced Data' module implementation.

--- js/ghg-data/ghg-data.js
+++ js/ghg-data/ghg-data.js
@@ -15,8 +15,9 @@ define(['jquery'], function ($) {
         /* Extend default configuration. */
         this.CONFIG = $.extend(true, {}, this.CONFIG, config);
 
-        var src = 'http://fenixapps.fao.org/repository/countrystat/view_ghg.html?tab=ghg_burned_areas_savanna';
-        var iframe = ' <iframe src="'+ src +'" width="100%" height="1700px" frameborder="0"></iframe> ';
+        // var src = 'http://fenixapps.fao.org/repository/countrystat/view_ghg.html?tab=ghg_burned_areas_savanna';
+        var src = 'http://168.202.28.214:7070/fnx_maps_playground/index_dev.html#distribution/en';
+        var iframe = ' <iframe src="'+ src +'" width="1000px" height="1700px" frameborder="0"></iframe> ';
         $('#tiles_container').html(iframe);
 
     };
