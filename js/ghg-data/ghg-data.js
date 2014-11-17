595bb04a528b3d2026be68fa11322007b1868396
Simone Murzilli <simone.murzilli@fao.org>
2014-11-17 16:45:59 +0100

    Update ghg-data.js

--- js/ghg-data/ghg-data.js
+++ js/ghg-data/ghg-data.js
@@ -16,7 +16,8 @@ define(['jquery'], function ($) {
         this.CONFIG = $.extend(true, {}, this.CONFIG, config);
 
         // var src = 'http://fenixapps.fao.org/repository/countrystat/view_ghg.html?tab=ghg_burned_areas_savanna';
-        var src = 'http://168.202.28.214:7070/fnx_maps_playground/index_dev.html#distribution/en';
+        //var src = 'http://168.202.28.214:7070/fnx_maps_playground/index_dev.html#distribution/en';
+        var src = 'http://168.202.39.41/fnx_maps/ghg.html#distribution/en';
         var iframe = ' <iframe src="'+ src +'" width="970px" height="1450px" frameborder="0"></iframe> ';
         $('#tiles_container').html(iframe);
 
