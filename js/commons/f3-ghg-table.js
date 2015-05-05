7070ff0d60e19e8619a2624e528398138c775dda
simonefenix <simone.murzilli@fao.org>
2015-05-05 09:24:50 +0200

    added export to table

--- js/commons/f3-ghg-table.js
+++ js/commons/f3-ghg-table.js
@@ -244,23 +244,16 @@
             return rows;
         }
 
-        function createExportButton(id) {
-            // add export button
-
-            // call exportTable on click
-
-        }
-
         function exportTableToCSV($table, filename) {
             var $headers = $table.find('tr:has(th)'),
                 $rows = $table.find('tr:has(td)')
 
-            // Temporary delimiter characters unlikely to be typed by keyboard
-            // This is to avoid accidentally splitting the actual contents
+                // Temporary delimiter characters unlikely to be typed by keyboard
+                // This is to avoid accidentally splitting the actual contents
                 ,tmpColDelim = String.fromCharCode(11) // vertical tab character
                 ,tmpRowDelim = String.fromCharCode(0) // null character
 
-            // actual delimiter characters for CSV format
+                // actual delimiter characters for CSV format
                 ,colDelim = '","'
                 ,rowDelim = '"\r\n"';
 
@@ -308,8 +301,6 @@
                 return $text.replace('"', '""'); // escape double quotes
             }
         }
-
-
 
         return {
             init: init,
