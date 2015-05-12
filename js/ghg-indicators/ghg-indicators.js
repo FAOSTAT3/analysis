37fa0cdd3c4335a7f85fc7834bef44cbdd770204
simonefenix <simone.murzilli@fao.org>
2015-05-12 09:29:06 +0200

    dirty fix to indicators

--- js/ghg-indicators/ghg-indicators.js
+++ js/ghg-indicators/ghg-indicators.js
@@ -5,16 +5,18 @@ define(['jquery'], function ($) {
     function GHG_INDICATORS() {
 
         this.CONFIG = {
+            placeholder: 'tiles_container',
             lang: 'E'
         }
 
     }
 
     GHG_INDICATORS.prototype.init = function(config) {
-
         /* Extend default configuration. */
         this.CONFIG = $.extend(true, {}, this.CONFIG, config);
-        $('#tiles_container').html('<h1>Indicators</h1><center><img style="width: 800px; height: 240px;" src="http://fenixapps2.fao.org/faostat-ghg/modules/tiled-analysis/images/ghg_indicators.png"></center>');
+
+        var html = "<div class='container-fluid'> <div class='row'><div class='col-xs-12'><h1>Indicators</h1><center><img style='width: 800px; height: 240px;' src='http://fenixapps2.fao.org/faostat-ghg/modules/tiled-analysis/images/ghg_indicators.png'></center></div></div>";
+        $('#'+ this.CONFIG.placeholder).html(html);
 
     };
 
