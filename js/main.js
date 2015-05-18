03068c467f754eb71f41c6ad195f2f2a19d504a2
Kalimaha <guido.barbaglia@gmail.com>
2015-05-18 12:04:05 +0200

    New QA/QC in FAOSTAT.

--- js/main.js
+++ js/main.js
@@ -5,20 +5,20 @@ require.config({
     baseUrl: 'js/libs',
 
     paths: {
-        'bootstrap': '//fenixapps.fao.org/repository/js/bootstrap/3.2/js/bootstrap.min',
-        'chosen': '//fenixapps.fao.org/repository/js/chosen/1.0.0/chosen.jquery.min',
-        'highcharts': '//fenixapps.fao.org/repository/js/highcharts/4.0.4/js/highcharts',
-        'jquery': '//code.jquery.com/jquery-1.10.1.min',
-        'mustache': '//cdnjs.cloudflare.com/ajax/libs/mustache.js/0.8.1/mustache',
-        'jquery.power.tip': '//fenixapps.fao.org/repository/js/jquery.power.tip/1.1.0/jquery.powertip.min',
-        'F3_CHART': 'analysis/js/libs/commons/f3-chart',
-        'GHG_QA_QC': 'analysis/js/ghg-qa-qc/ghg-qa-qc',
-        'GHG_OVERVIEW': 'analysis/js//ghg-overview/ghg-overview',
-        'TILESMGR': '../tiles-manager/tiles-manager',
-        'jquery.sticky': 'stickyjs/1.0/jquery.sticky',
-        'ANALYSIS_GHG_QA_QC': '../ghg-qa-qc/ghg-qa-qc',
-        'highcharts': repository + 'highcharts/4.0.4/js/highcharts',
-        'highcharts_exporting' : repository + 'highcharts/4.0.4/js/modules/exporting'
+        //'bootstrap': '//fenixapps.fao.org/repository/js/bootstrap/3.2/js/bootstrap.min',
+        //'chosen': '//fenixapps.fao.org/repository/js/chosen/1.0.0/chosen.jquery.min',
+        //'highcharts': '//fenixapps.fao.org/repository/js/highcharts/4.0.4/js/highcharts',
+        //'jquery': '//code.jquery.com/jquery-1.10.1.min',
+        //'mustache': '//cdnjs.cloudflare.com/ajax/libs/mustache.js/0.8.1/mustache',
+        //'jquery.power.tip': '//fenixapps.fao.org/repository/js/jquery.power.tip/1.1.0/jquery.powertip.min',
+        //'F3_CHART': 'analysis/js/libs/commons/f3-chart',
+        //'GHG_QA_QC': 'analysis/js/ghg-qa-qc/ghg-qa-qc',
+        //'GHG_OVERVIEW': 'analysis/js//ghg-overview/ghg-overview',
+        //'TILESMGR': '../tiles-manager/tiles-manager',
+        //'jquery.sticky': 'stickyjs/1.0/jquery.sticky',
+        //'ANALYSIS_GHG_QA_QC': '../ghg-qa-qc/ghg-qa-qc',
+        //'highcharts': repository + 'highcharts/4.0.4/js/highcharts',
+        //'highcharts_exporting' : repository + 'highcharts/4.0.4/js/modules/exporting'
     },
 
     shim: {
